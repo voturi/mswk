150771698d495fa234ebb8d50bfaee7abc330f8b
Praveen Voturi <praveen.voturi@service.nsw.gov.au>
2023-02-21 13:00:35 +1100

    First Commit

---
+++ src/main/kotlin/com/voturi/mswk/MswkApplication.kt
@@ -0,0 +1,11 @@
+package com.voturi.mswk
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MswkApplication
+
+fun main(args: Array<String>) {
+	runApplication<MswkApplication>(*args)
+}
